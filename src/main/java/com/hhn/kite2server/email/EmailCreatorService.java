21ae19f1cc084c8dd42237b098a35f2be21613e5
Mergim Miftari <mmiftari@stud.hs-heilbronn.de>
2023-10-06 10:50:10 +0200

    Fixed bug and removed commented code.

--- src/main/java/com/hhn/kite2server/email/EmailCreatorService.java
+++ src/main/java/com/hhn/kite2server/email/EmailCreatorService.java
@@ -1,7 +1,6 @@
 package com.hhn.kite2server.email;
 
 import lombok.AllArgsConstructor;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +8,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @AllArgsConstructor
 @Service
 public class EmailCreatorService {
-
     private final ResourceLoader resourceLoader;
 
     public String buildEmailConfirmRegistrationAddress(String link) {
@@ -36,18 +32,6 @@ public class EmailCreatorService {
         }
     }
 
-    //public String buildEmailConfirmRegistrationAddress(String link) {
-    //    try {
-    //        Resource resource = resourceLoader.getResource("classpath:templates/confirm_address_email.html");
-    //        String email = new String(Files.readAllBytes(Paths.get(resource.getURI())));
-    //        email = email.replace("{link}", link);
-    //        return email;
-    //    } catch (IOException e) {
-    //        e.printStackTrace();
-    //        return null;
-    //    }
-    //}
-
     public String buildEmailConfirmResetOfPassword(String link) {
         try {
             InputStream inputStream = getClass().getResourceAsStream("/templates/email_confirm_reset.html");
@@ -66,18 +50,6 @@ public class EmailCreatorService {
         }
     }
 
-    //public String buildEmailConfirmResetOfPassword(String link) {
-    //    try {
-    //       Resource resource = resourceLoader.getResource("classpath:templates/email_confirm_reset.html");
-    //        String email = new String(Files.readAllBytes(Paths.get(resource.getURI())));
-    //        email = email.replace("{link}", link);
-    //        return email;
-    //    } catch (IOException e) {
-    //        e.printStackTrace();
-    //        return null;
-    //    }
-    //}
-
     public String buildEmailInformAboutNewPassword(String newPassword) {
         try {
             InputStream inputStream = getClass().getResourceAsStream("/templates/new_password_email.html");
@@ -95,16 +67,4 @@ public class EmailCreatorService {
             return null;
         }
     }
-
-    //public String buildEmailInformAboutNewPassword(String newPassword) {
-    //    try {
-    //        Resource resource = resourceLoader.getResource("classpath:templates/new_password_email.html");
-    //        String email = new String(Files.readAllBytes(Paths.get(resource.getURI())));
-    //        email = email.replace("{password}", newPassword);
-    //        return email;
-    //    } catch (IOException e) {
-    //        e.printStackTrace();
-    //        return null;
-    //    }
-    //}
 }
