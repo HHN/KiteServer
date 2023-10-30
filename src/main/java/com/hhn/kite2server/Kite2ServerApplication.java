7fd2d8c0858e760b7345476e86bee86fe48ff165
S3nnar <privat.mc@t-online.de>
2023-10-30 14:06:33 +0100

    removed custom logger and implemented spring logger vis yml-file

--- src/main/java/com/hhn/kite2server/Kite2ServerApplication.java
+++ src/main/java/com/hhn/kite2server/Kite2ServerApplication.java
@@ -1,10 +1,11 @@
 package com.hhn.kite2server;
 
-import com.hhn.kite2server.logger.LoggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.hhn.kite2server.*")
@@ -15,9 +16,7 @@ public class Kite2ServerApplication {
         try{
             SpringApplication.run(Kite2ServerApplication.class, args);
         } catch (Exception e) {
-            LoggerConfig loggerConfig = new LoggerConfig();
-            loggerConfig.configureLogger();
-            loggerConfig.getLogger().severe("Error encounter: " + e.getMessage());
+
         }
     }
 
