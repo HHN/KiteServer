d47270f35a07a4f602f5ef94bd6a475dac6b931e
Mergim Miftari <mmiftari@stud.hs-heilbronn.de>
2024-01-03 15:01:38 +0100

    Changed the GPT Version and also fixed some issues.

--- src/main/java/com/hhn/kite2server/Kite2ServerApplication.java
+++ src/main/java/com/hhn/kite2server/Kite2ServerApplication.java
@@ -5,19 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 @EnableJpaRepositories("com.hhn.kite2server.*")
 @EntityScan("com.hhn.kite2server.*")
 public class Kite2ServerApplication {
 
     public static void main(String[] args) {
-        try{
-            SpringApplication.run(Kite2ServerApplication.class, args);
-        } catch (Exception e) {
-
-        }
+        SpringApplication.run(Kite2ServerApplication.class, args);
     }
-
 }
