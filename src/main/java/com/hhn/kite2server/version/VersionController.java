a00695dae3538315941145773d7d9147dec5e7f6
Mergim Miftari <Mergim-Miftari@hotmail.de>
2024-07-08 14:01:07 +0200

    Updated build number

--- src/main/java/com/hhn/kite2server/version/VersionController.java
+++ src/main/java/com/hhn/kite2server/version/VersionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class VersionController {
 
-    private static int VERSION = 29;
+    private static int VERSION = 30;
 
     @GetMapping
     public Response getVersion() {
