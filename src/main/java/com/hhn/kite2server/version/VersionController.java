6930dd068ad3a27d1fad3377491ba3750c4e9de1
Mergim Miftari <Mergim-Miftari@hotmail.de>
2024-07-10 09:13:14 +0200

    Updated version number for deployment

--- src/main/java/com/hhn/kite2server/version/VersionController.java
+++ src/main/java/com/hhn/kite2server/version/VersionController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class VersionController {
 
-    private static int VERSION = 30;
-
+    private static int VERSION = 31;
+    
     @GetMapping
     public Response getVersion() {
         Response response = new Response();
